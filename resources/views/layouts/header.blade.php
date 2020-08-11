f2b6fbc385df90ad8ccd31d9435e8fd7401397b8
Jens Twesmann <j.twesmann@star-media.biz>
2020-08-11 12:15:54 +0200

    added Text

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -24,9 +24,9 @@
                 <div class="col-md-12">
                     <a href="{{ route('home') }}" class="ritekhed-logo d-none d-sm-block"> <img src="/images/logo.png" alt="BC70 Soest Logo"> </a>
                     <a href="{{ route('home') }}" class="d-block d-sm-none"> <img src="/images/logo.png" alt="BC70 Soest Logo" width="60%" style="float: left; margin-top: 5px;"> </a>
-                    <a href="https://www.umzug-hitzke.de" class="hidden-xs hidden-sm d-none d-sm-block"> <img src="/images/Hitzke_Logo_quer.jpg" alt="Umzug Hitzke" width="200px" style="float: right; margin-top: 27px;"> </a>
-                    <a href="https://www.umzug-hitzke.de" class="hidden-xs hidden-sm d-block d-sm-none"> <img src="/images/Hitzke_Logo_quer.jpg" alt="Umzug Hitzke" width="80px" style="float: right; margin-top: 5px;"> </a>
-                    <div class="ritekhed-right-head d-block d-sm-none" style="position: absolute !important; right: 5px; top: 20px;">
+                    <a href="https://www.umzug-hitzke.de" class="hidden-xs hidden-sm d-none d-sm-block" title="Hauptsponsor Umzug Hitzke" style="color: #fff; float: right; margin-top: 27px;"> Hauptsponsor <br><img src="/images/Hitzke_Logo_quer.jpg" alt="Umzug Hitzke" width="200px"></a>
+                    <a href="https://www.umzug-hitzke.de" class="hidden-xs hidden-sm d-block d-sm-none"  title="Hauptsponsor Umzug Hitzke" style="color: #fff; float: right; margin-top: 5px;">Hauptsponsor <br> <img src="/images/Hitzke_Logo_quer.jpg" alt="Umzug Hitzke" width="80px"> </a>
+                    <div class="ritekhed-right-head d-block d-sm-none" style="position: absolute !important; right: 5px; top: 40px;">
                                 <span class="ritekhed-menu-link">
                                     <span class="menu-bar"></span>
                                     <span class="menu-bar"></span>
