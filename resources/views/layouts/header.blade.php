841b34b94f6ee31ef3982339182431169175cac5
Jens Twesmann <j.twesmann@star-media.biz>
2020-09-11 11:02:33 +0200

    fixed menu issue

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <ul class="ritekhed-top-strip-social">
+                    <ul class="ritekhed-top-strip-social d-none d-lg-block ">
                         <li><a href="https://www.facebook.com/Bc70Soest" class="fab fa-facebook-f"></a></li>
                         <li><a href="https://www.twitter.com/bc70soest" class="fab fa-twitter"></a></li>
                     </ul>
@@ -23,25 +23,16 @@
             <div class="row">
                 <div class="col-md-12">
                     <a href="{{ route('home') }}" class="ritekhed-logo d-none d-sm-block"> <img src="/images/logo.png" alt="BC70 Soest Logo"> </a>
+                    <span class="ritekhed-menu-link" style="position: absolute !important; left: 10px; top: 40px;">
+                                    <span class="menu-bar"></span>
+                                    <span class="menu-bar"></span>
+                                    <span class="menu-bar"></span>
+                                </span>
                     <a href="{{ route('home') }}" class="d-block d-sm-none"> <img src="/images/logo.png" alt="BC70 Soest Logo" width="60%" style="float: left; margin-top: 5px;"> </a>
                     <a href="https://www.umzug-hitzke.de" class="hidden-xs hidden-sm d-none d-sm-block" title="Hauptsponsor Umzug Hitzke" style="color: #fff; float: right; margin-top: 27px;"> Hauptsponsor <br><img src="/images/Hitzke_Logo_quer.jpg" alt="Umzug Hitzke" width="200px"></a>
                     <a href="https://www.umzug-hitzke.de" class="hidden-xs hidden-sm d-block d-sm-none"  title="Hauptsponsor Umzug Hitzke" style="color: #fff; float: right; margin-top: 5px;">Hauptsponsor <br> <img src="/images/Hitzke_Logo_quer.jpg" alt="Umzug Hitzke" width="80px"> </a>
-                    <div class="ritekhed-right-head d-block d-sm-none" style="position: absolute !important; right: 5px; top: 40px;">
-                                <span class="ritekhed-menu-link">
-                                    <span class="menu-bar"></span>
-                                    <span class="menu-bar"></span>
-                                    <span class="menu-bar"></span>
-                                </span>
-                        <nav id="main-nav">
-                            {!! $menu !!}
-                        </nav>
-                    </div>
-                    <div class="ritekhed-right-head d-none d-sm-block">
-                                <span class="ritekhed-menu-link">
-                                    <span class="menu-bar"></span>
-                                    <span class="menu-bar"></span>
-                                    <span class="menu-bar"></span>
-                                </span>
+                    <div class="ritekhed-right-head">
+
                         <nav id="main-nav">
                             {!! $menu !!}
                         </nav>
